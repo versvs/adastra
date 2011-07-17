d62c68b7d6d334e07d70e8940cb4f9088485755e
Jose Alcántara <versvs@gmail.com>
2011-07-17 12:03:14 -0300

    quitando más personalizaciones

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<div class="titulofecha">
 			<h2 class="titulo" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 			<span class="fecha"><?php the_time('Y-m-d'); ?> <?php _e("@"); ?> <?php the_time('H:i'); ?></span>
-			<span class="meta">Publicado por <a href="mailto:<?php echo antispambot(get_the_author_email()); ?>"><?php the_author(); ?></a>. Filled under: <?php the_category(',') ?> <?php the_tags('Tags: ', ', ', ''); ?> &bull; <?php comments_popup_link('No Comments','1 Comment','% Comments'); ?> <?php edit_post_link(' &mdash; (Edit this?)'); ?></span>
+			<span class="meta">Published by <a href="mailto:<?php echo antispambot(get_the_author_email()); ?>"><?php the_author(); ?></a>. Filled under: <?php the_category(',') ?> <?php the_tags('Tags: ', ', ', ''); ?> &bull; <?php comments_popup_link('No Comments','1 Comment','% Comments'); ?> <?php edit_post_link(' &mdash; (Edit this?)'); ?></span>
 	</div>
 	
 <div class="main">
