09b5ab0a6d48592ee3dd0211392f7f9f8ae08cff
Jose Alcántara <versvs@gmail.com>
2011-07-17 11:51:03 -0300

    eliminada personalización previa al empaquetado definitivo

--- footer.php
+++ footer.php
@@ -3,6 +3,5 @@
 <div id="footer">
 Tema <em>Ad Astra</em> por <a href="http://www.versvs.net/" title="Jose Alcántara" rel="follow">Jose Alcántara</a> &bull;  Lees esto gracias a <a href="http://wordpress.org">WordPress</a>
 
-<div id="devolucion" style="margin-top: 3px"><img width="40px" src="/wp-content/themes/adastra/imagenes/devolucion.png" alt=""/></div>
 </div>
 
