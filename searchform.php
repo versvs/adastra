d73bbc9be2d219877b3454f393bc43d4cc84ae2a
versvs <versvs@gmail.com>
2011-08-07 15:40:38 -0300

    quitado label a searchform

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-	<label for="s">Búsqueda</label>
 	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
 	<input id="searchsubmit" type="image" src="<?php bloginfo('template_directory'); ?>/imagenes/icono-ir.png" value="Buscar"  />
 </form>
