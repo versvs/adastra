ea7d46debadee94172adc794509d3b915764b74c
versvs <versvs@gmail.com>
2011-10-15 14:02:43 +0200

    corregido pie

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div>
 
 <div id="footer">
-<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &bull; Tema <a href="http://adastraerrans.com/ad-astra"><em>Ad Astra</em></a> por <a href="http://www.versvs.net/" title="Jose Alcántara" rel="follow">Jose Alcántara</a> &bull;  Lees esto gracias a <a href="http://wordpress.org">WordPress</a>
+<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &bull; Tema <a href="http://adastraerrans.com/ad-astra"><em>Ad Astra</em></a> por <a href="http://www.versvs.net/" title="Jose Alcántara" rel="follow">Jose Alcántara</a> para <a title="Consultoría y Blogs, Madrid" href="http://www.cartograf.net">Cartograf</a> &bull;  Lees esto gracias a <a href="http://wordpress.org">WordPress</a>
 
 </div>
 
