b1b0c8b7db08e9001ae6030c2a82ba3b96ec6f03
versvs <versvs@gmail.com>
2012-03-01 21:48:11 +0100

    añadida clase 404, mas posts a las queries

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div id="contenido">
+<div id="contenido" class="404">
 	<div class="post clearfix">
 		<h3 style="text-align:center">
 			No hemos encontrado lo que buscabas.
@@ -9,22 +9,43 @@
 			</a>
 		</h3>
 
+		<h3>Puedes hacer una búsqueda...</h3>
 		<?php get_template_part('searchform'); ?>
 
-	<ul>
-		<?php
-			global $post;
-			$args = array( 'numberposts' => 5);
-			$myposts = get_posts( $args );
-			foreach( $myposts as $post ) :	setup_postdata($post); ?>
-				<li>
-					<a href="<?php the_permalink(); ?>">
-						<?php the_title(); ?>
-					</a>
-				</li>
-		<?php endforeach; ?>
-	</ul>
-
+		<h3>o quizá te interesen los siguientes posts:</h3>
+		<div id="recientes">
+			<h3>Posts recientes</h3>
+			<ul>
+				<?php
+					global $post;
+					$args = array( 'numberposts' => 7);
+					$myposts = get_posts( $args );
+					foreach( $myposts as $post ) :	setup_postdata($post); ?>
+						<li>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	
+		<div id="comentados">
+			<h3>Posts más comentados</h3>
+			<ul>
+				<?php
+					global $post;
+					$args = array( 'numberposts' => 7, 'orderby' => 'comment_count');
+					$myposts = get_posts( $args );
+					foreach( $myposts as $post ) :	setup_postdata($post); ?>
+						<li>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
 
 	</div>
 
