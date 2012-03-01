525e03c1d6297d782709152e7032e1149b63a8f2
versvs <versvs@gmail.com>
2012-03-01 21:19:28 +0100

    arreglado 404

--- 404.php
+++ 404.php
@@ -1,10 +1,15 @@
 <?php get_header(); ?>
-<div class="post">
-<h3 style="text-align:center">Not Found! <a href="<?php bloginfo('url'); ?>">Go back home?</a></h3>
 
-<?php get_template_part('searchform'); ?>
+<div id="contenido">
+	<div class="post">
+		<h3 style="text-align:center">Not Found! <a href="<?php bloginfo('url'); ?>">Go back home?</a></h3>
 
+		<?php get_template_part('searchform'); ?>
+
+	</div>
+	
 </div>
+	
 <?php get_footer(); ?>
 
 
