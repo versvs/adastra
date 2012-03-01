dfc167d33eeee6ac3b0a88251a5941e677b793b9
versvs <versvs@gmail.com>
2012-03-01 22:14:24 +0100

    añadido clearfix a varios div en index

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div class="post">
-	<div class="titulofecha">
+<div class="post clearfix">
+	<div class="titulofecha clearfix">
 			<h2 class="titulo" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 			<span class="fecha"><?php the_time('Y-m-d'); ?> <?php _e("@"); ?> <?php the_time('H:i'); ?></span>
 			<span class="meta">Publicado por <a href="<?php get_the_author_url(); ?>"><?php the_author(); ?></a>. Archivado en: <?php the_category(',') ?>. <?php the_tags('Etiquetado como: ', ', ', ''); ?> &bull; <?php comments_popup_link('Aún no hay comentarios','1 comentario','% comentarios'); ?> <?php edit_post_link(' &mdash; (¿Editar?)'); ?></span>
@@ -22,7 +22,7 @@
     <?php if ( comments_open() ) comments_template(); ?>
 
 <?php endwhile; else: ?>
-<?php include (TEMPLATEPATH . '/404.php'); ?>
+<?php get_template_part('404'); ?>
 <?php endif; ?>
 
 <div class="paginado">
