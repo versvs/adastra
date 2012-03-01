cc4020c72a85c8216d7c75c4fa923d77ab8f226d
versvs <versvs@gmail.com>
2012-03-01 21:25:19 +0100

    más 404

--- 404.php
+++ 404.php
@@ -2,15 +2,22 @@
 
 <div id="contenido">
 	<div class="post">
-		<h3 style="text-align:center">Not Found! <a href="<?php bloginfo('url'); ?>">Go back home?</a></h3>
+		<h3 style="text-align:center">
+			No hemos encontrado lo que buscabas.
+			<a href="<?php bloginfo('url'); ?>">
+				Volver al inicio
+			</a>
+		</h3>
 
 		<?php get_template_part('searchform'); ?>
 
 	</div>
+
 	
+	<?php get_footer(); ?>
+
+
 </div>
-	
-<?php get_footer(); ?>
 
 
 <?php get_template_part('subfooter'); ?>
