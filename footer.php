b5e03984265019b0515fdffe2e15a8fff715baf1
versvs <versvs@gmail.com>
2012-04-27 00:36:12 +0200

    más wrapperapocalipsis

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
+<div id="footer-container" class="footer container clearfix">
+	<div id="footer-wrapper" class="footer wrapper clearfix">
+		<a href="<?php bloginfo('url'); ?>">
+			<?php bloginfo('name'); ?>
+		</a>
+		&bull;
+		Tema <a href="http://adastraerrans.com/ad-astra">
+				<em>Ad Astra</em>
+			  </a>
+		por
+		<a href="http://www.versvs.net/" title="Jose Alcántara" rel="follow">
+			Jose Alc&aacute;ntara
+		</a>
+		para
+		<a title="Inteligencia y an&aacute;lisis de redes, Madrid" href="http://www.cartograf.net">
+			Cartograf
+		</a>
+		&bull;
+		Lees esto gracias a <a href="http://wordpress.org">WordPress</a>
+	</div>
 </div>
-
-<div id="footer">
-<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &bull; Tema <a href="http://adastraerrans.com/ad-astra"><em>Ad Astra</em></a> por <a href="http://www.versvs.net/" title="Jose Alcántara" rel="follow">Jose Alcántara</a> para <a title="Consultoría y Blogs, Madrid" href="http://www.cartograf.net">Cartograf</a> &bull;  Lees esto gracias a <a href="http://wordpress.org">WordPress</a>
-
-</div>
-
