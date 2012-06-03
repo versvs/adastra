d2b2882bbad8153bf335d0ccbbe4b30ca1b2379a
versvs <versvs@gmail.com>
2012-06-03 23:44:56 +0200

    añadido recomendado.php

---
+++ recomendado.php
@@ -0,0 +1,37 @@
+	<h3>Puedes hacer una b&uacute;squeda...</h3>
+		<?php get_template_part('searchform'); ?>
+
+		<h3>o quiz&aacute; te interesen los siguientes posts:</h3>
+		<div id="recientes">
+			<h3>Posts recientes</h3>
+			<ul>
+				<?php
+					global $post;
+					$args = array( 'numberposts' => 7);
+					$myposts = get_posts( $args );
+					foreach( $myposts as $post ) :	setup_postdata($post); ?>
+						<li>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	
+		<div id="comentados">
+			<h3>Posts m&aacute; comentados</h3>
+			<ul>
+				<?php
+					global $post;
+					$args = array( 'numberposts' => 7, 'orderby' => 'comment_count');
+					$myposts = get_posts( $args );
+					foreach( $myposts as $post ) :	setup_postdata($post); ?>
+						<li>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
