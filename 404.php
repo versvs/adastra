c148ea024ab78ab8ea621f00f469cc13de7197ab
versvs <versvs@gmail.com>
2012-06-03 23:44:18 +0200

    simplificado 404.php

--- 404.php
+++ 404.php
@@ -13,43 +13,7 @@
 			</a>
 		</h3>
 
-		<h3>Puedes hacer una búsqueda...</h3>
-		<?php get_template_part('searchform'); ?>
-
-		<h3>o quizá te interesen los siguientes posts:</h3>
-		<div id="recientes">
-			<h3>Posts recientes</h3>
-			<ul>
-				<?php
-					global $post;
-					$args = array( 'numberposts' => 7);
-					$myposts = get_posts( $args );
-					foreach( $myposts as $post ) :	setup_postdata($post); ?>
-						<li>
-							<a href="<?php the_permalink(); ?>">
-								<?php the_title(); ?>
-							</a>
-						</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
-	
-		<div id="comentados">
-			<h3>Posts más comentados</h3>
-			<ul>
-				<?php
-					global $post;
-					$args = array( 'numberposts' => 7, 'orderby' => 'comment_count');
-					$myposts = get_posts( $args );
-					foreach( $myposts as $post ) :	setup_postdata($post); ?>
-						<li>
-							<a href="<?php the_permalink(); ?>">
-								<?php the_title(); ?>
-							</a>
-						</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
+		<?php get_template_part('recomendado'); ?>
 
 	</div>
 
