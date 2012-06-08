6746557a981fb1b14e78c13303d3f152c1bb5ee0
Jose Alcántara <versvs@gmail.com>
2012-06-08 23:33:57 +0200

    get_the_date

--- search.php
+++ search.php
@@ -38,7 +38,7 @@
 						
 					<span class="post-date clearfix">
 						<a title="Enlace permanente para <?php the_title(); ?>" href="<?php the_permalink() ?>" rel="bookmark">
-							<?php get_the_date(); ?>
+							<?php echo get_the_date(); ?>
 							<?php _e("@"); ?>
 							<?php the_time('H:i'); ?>
 						</a>
