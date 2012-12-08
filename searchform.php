9344130f030304cb6e89ba6b44e9a3b408c941b4
Jose Alcántara <versvs@gmail.com>
2012-12-08 21:48:30 +0100

    validando search form

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-	<input id="searchsubmit" type="image" src="<?php bloginfo('template_directory'); ?>/imagenes/icono-ir.png" value="Buscar" alt="Buscar" />
+	<input id="searchsubmit" type="image" src="<?php bloginfo('template_directory'); ?>/imagenes/icono-ir.png" alt="Buscar" />
 </form>
