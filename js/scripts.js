e5a8f1f589f886f7c2052611d601c58f947afd2f
Jose Alcántara <jose@cartograf.net>
2014-01-07 20:17:36 +0100

    Correcciones al fixed del menú y a la meta de las pastillas

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
+$(function() {
+	var pull 		= $('#pull');
+		menu 		= $('#nav ul');
+		menuHeight	= menu.height();
+
+	$(pull).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function(){
+  		var w = $(window).width();
+  		if(w > 320 && menu.is(':hidden')) {
+  			menu.removeAttr('style');
+  		}
+ 		});
+});
+
 $(document).ready(function() {
+	
+	
+	$(window).scroll(function (){
+		if ($(window).scrollTop()>40){
+			$('#nav-container').addClass('topped');
+		}else{
+			$('#nav-container').removeClass('topped');
+		} 	
+	});
 	
 	$(window).scroll(function (){
 		if ($(window).scrollTop()>200){
